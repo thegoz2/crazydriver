9a60d05d18dd1531614edb4ade226f6541dce2ba
Mahakit1 <Mahakit2543@hotmail.com>
2020-08-29 16:10:21 +0700

    update packing
    updating packing

--- Packing/Assets/killenter.cs
+++ Packing/Assets/killenter.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine;using UnityEngine.UI;
 
 public class killenter : MonoBehaviour
 {
+    public RawImage bar;
+    private bool isShowing = false;
+
     void OnTriggerEnter(Collider other)
     {
+        isShowing = !isShowing;
+        bar.enabled = true;
         Destroy(gameObject);
     }
 }
