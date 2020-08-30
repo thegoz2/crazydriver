e818c22f5ba5d3d0b15d3ccaf137d5e924f3c170
Mahakit1 <Mahakit2543@hotmail.com>
2020-08-30 15:39:17 +0700

    1

---
+++ gameplay packing/Assets/weightcouter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class weightcouter : MonoBehaviour
+{
+    public float currentweight;
+    public Text weightshow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        weightshow.text = currentweight.ToString() + "kg";
+    }
+}
