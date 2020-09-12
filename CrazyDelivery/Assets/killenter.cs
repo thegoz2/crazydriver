237129e387a6ddaf3fa4c5827d824fd3f039ed09
kittiwat <dukes03@users.noreply.github.com>
2020-09-12 09:49:23 +0700

    sum all file
    ขาดpack
    ขาด upgrade

---
+++ CrazyDelivery/Assets/killenter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;using UnityEngine.UI;
+
+public class killenter : MonoBehaviour
+{
+    public RawImage bar;
+    private bool isShowing = false;
+    public float weight;
+    public Text weightshower;
+    private weightcouter currentweightcouter;
+
+    void Start()
+    {
+        currentweightcouter = (weightcouter)FindObjectOfType(typeof(weightcouter));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        isShowing = !isShowing;
+        bar.enabled = true;
+        currentweightcouter.currentweight += weight;
+        Destroy(gameObject);
+    }
+}
